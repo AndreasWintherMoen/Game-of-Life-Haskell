024fa3c958674bed6d5ffeed7a9a95343af7bd2d
AndreasWintherMoen <andreas@winthermoen.no>
2020-04-07 19:45:48 +0200

    Simulate board on timed interval Currently the 'simulation' only flips all cells

--- src/Game.hs
+++ src/Game.hs
@@ -2,6 +2,7 @@ module Game where
 
 import Data.Array
 import Graphics.Gloss.Interface.Pure.Game
+import Data.Time.Clock
 
 
 data State = Placing | Simulating
@@ -10,8 +11,27 @@ type Board = Array (Int, Int) Cell
 
 data Game = Game { gameState :: State
                  , gameBoard :: Board
+                 , startTime
+                 , timeLastUpdate
+                 , time :: UTCTime
                  }
 
+seconds2DiffTime :: Float -> NominalDiffTime
+seconds2DiffTime = realToFrac 
+diffTime2seconds :: NominalDiffTime -> Float 
+diffTime2seconds = realToFrac 
+
+timeSinceLastUpdate :: Game -> Float
+timeSinceLastUpdate game = diffTime2seconds $ diffUTCTime (time game) (timeLastUpdate game)
+
+updateTimeLastUpdate :: Game -> Game
+updateTimeLastUpdate game = game { timeLastUpdate = time game }
+
+incrementTime :: Float -> Game -> Game
+incrementTime dt game = game {time = addUTCTime (seconds2DiffTime dt) $ time game}
+
+timePerSimulation :: Float
+timePerSimulation = 0.5
 
 cols :: Int
 cols = 30
@@ -36,9 +56,12 @@ isCellInBounds (x, y) = x >= 0 && x < cols && y >= 0 && y < rows
 
 isCoordCorrect = inRange ((0, 0), (rows - 1, cols - 1))
 
-initialGame :: Game
-initialGame = Game { gameState = Placing
+initialGame :: UTCTime -> Game
+initialGame t0 = Game { gameState = Placing
                    , gameBoard = array indexRange $ zip (range indexRange) (repeat False)
+                   , startTime = t0
+                   , timeLastUpdate = t0
+                   , time = t0
                    }
     where indexRange = ((0, 0), (rows, cols))
 
@@ -70,7 +93,9 @@ numNeighbors :: Board -> (Int, Int) -> Int
 numNeighbors board (x, y) = sumCells [board ! (i, j) | i <- [x-1..x+1], j <- [y-1..y+1], isCellInBounds (i, j)]
 
 simulateBoard :: Board -> Board
-simulateBoard board = board
+-- simulateBoard board = board
+simulateBoard board = array indexRange $ concatMap (\x -> [(x, not (isCellFilled board x))]) (range indexRange)
+    where indexRange = ((0, 0), (rows, cols))
 
 simulateStep :: Game -> Game
 simulateStep game = game { gameBoard = simulateBoard (gameBoard game) }
@@ -83,3 +108,13 @@ transformGame (EventKey (MouseButton LeftButton) Up _ mousePos) game =
         Placing -> flipStartCell game $ mousePosAsCellCoord mousePos
         Simulating -> game
 transformGame _ game = game
+
+simulateGame :: Float -> Game -> Game
+simulateGame dt game = 
+    case gameState game of 
+        Placing -> game 
+        Simulating -> 
+            if (timeSinceLastUpdate game) > timePerSimulation
+                then simulateStep $ updateTimeLastUpdate $ newGame 
+                else newGame
+                    where newGame = incrementTime dt game 
--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Data.Color
+import Data.Time.Clock
 
 import Game
 import Rendering
@@ -10,5 +11,8 @@ import Rendering
 window = InWindow "Game of Life" (screenWidth, screenHeight) (100, 100)
 
 main :: IO ()
-main = play window Rendering.backgroundColor 30 Game.initialGame Rendering.gameAsPicture Game.transformGame (const id)
+main = do
+    t0 <- getCurrentTime
+    play window Rendering.backgroundColor 30 (Game.initialGame t0) Rendering.gameAsPicture Game.transformGame Game.simulateGame
+
 
